929f81c178ba1949d13f39e866d3b1cb8b316f5f
hadishamgholi <hadishamgholi74@gmail.com>
2018-03-06 20:43:37 +0330

    first commit, with GUI
    Signed-off-by: hadishamgholi <hadishamgholi74@gmail.com>

---
+++ PersianAnagram/DataSetBuilder.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    public class DataSetBuilder
+    {
+        private static int primeNum = 2;
+        private string persianLetter = "آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی";
+        public Dictionary<char, int> LetterPrimeMap;
+        public Dictionary<string, int> WordPrimeMap;
+
+        public DataSetBuilder()
+        {
+            this.WordPrimeMap = new Dictionary<string, int>();
+            this.LetterPrimeMap = new Dictionary<char, int>();
+            PrepareLetterPrimeMap();
+            PrepareWordPrimeMap();
+        }
+
+        public List<string> GetAnagramOf(string input)
+        {
+            int mul = this.GenaratePrimeForWord(input);
+            var q = from m in this.WordPrimeMap where m.Value == mul select m.Key;
+            return q.ToList();
+        }
+
+        public void PrepareWordPrimeMap()
+        {
+            // reading from file
+            var fileName =  Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())) + "/words.csv";
+//            Console.WriteLine(fileName);
+            using (var sr = new System.IO.StreamReader(fileName))
+            {
+                string word;
+                while ((word = sr.ReadLine()) != null)
+                {
+                    WordPrimeMap[word] = GenaratePrimeForWord(word);
+                }
+            }
+        }
+
+        private int GenaratePrimeForWord(string word)
+        {
+            int mul = 1;
+            foreach (char c in word)
+            {
+                if (!LetterPrimeMap.ContainsKey(c))
+                {
+                    LetterPrimeMap[c] = GetNextPrimeNum();
+                }
+                mul = mul * LetterPrimeMap[c];
+            }
+
+            return mul;
+        }
+
+        private void PrepareLetterPrimeMap()
+        {
+            foreach (char c in persianLetter)
+            {
+                LetterPrimeMap.Add(c, GetNextPrimeNum());
+            }
+        }
+
+        private int GetNextPrimeNum()
+        {
+            if (primeNum == 2)
+            {
+                primeNum++;
+                return 2;
+            }
+
+            while (!IsPrime(primeNum))
+            {
+                ++primeNum;
+            }
+
+            return primeNum++;
+        }
+        
+        private bool IsPrime(int num)
+        {
+            double squaredNumber = Math.Sqrt(Convert.ToDouble(num));
+            for (var i = 2; i <= squaredNumber; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ PersianAnagram/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    using Gtk;
+    public class Window1 : Window
+    {
+        private Label label;
+        private Label result;
+        private static DataSetBuilder dataBuilder;
+        private Entry entry;
+ 
+        public Window1() : base("Center")
+        {
+            BorderWidth = 10;
+            SetDefaultSize(300, 300);
+            SetPosition(WindowPosition.Center);
+            DeleteEvent += delegate { Application.Quit(); };
+            
+            label = new Label("Enter a word:");
+            result = new Label("");
+            entry = new Entry();
+//            entry.Changed += OnChanged;
+        
+            VBox vbox = new VBox(false, 5);
+            HBox hbox = new HBox(true, 3);
+            HBox le = new HBox(false, 10);
+            
+            le.Add(label);
+            le.Add(entry);
+            
+            Alignment leAlignment = new Alignment(0, 0, 0, 0);
+            leAlignment.Add(le);
+            vbox.PackStart(leAlignment, false, true, 1);
+            vbox.Add(result);
+            Alignment valign = new Alignment(0, 1, 0, 0);
+//            valign.Add(result);
+            vbox.PackStart(valign, true, false, 1);
+        
+            Button anagramBtn = new Button("Anagram");
+            anagramBtn.SetSizeRequest(70, 30);
+            Button close = new Button("Close");
+            close.Clicked += OnCloseClicked;
+            anagramBtn.Clicked += OnAnagramClicked;
+        
+            hbox.Add(anagramBtn);
+            hbox.Add(close);
+        
+            Alignment halign = new Alignment(1, 0, 0, 0);
+            halign.Add(hbox);
+            
+        
+            vbox.PackStart(halign, false, false, 3);
+
+            Add(vbox);
+
+            ShowAll();
+        }
+        
+        void OnCloseClicked(object sender, EventArgs args)
+        {
+//            Entry entry = (Entry) sender;
+//            label.Text = entry.Text;
+            Application.Quit();
+        }
+
+        void OnAnagramClicked(object sender, EventArgs args)
+        {
+            Button anagram = (Button) sender;
+            result.Text = "";
+            anagram.Sensitive = false;
+            string s = entry.Text;
+            if (!string.IsNullOrEmpty(s))
+            {
+                dataBuilder.GetAnagramOf(s).ForEach(x => result.Text = result.Text + x + "\n");
+            }
+
+            anagram.Sensitive = true;
+        }
+
+
+        public static void Main()
+        {
+            dataBuilder = new DataSetBuilder();
+            Application.Init();
+            new Window1();
+            Application.Run();
+        }
+    
+    }
+}
